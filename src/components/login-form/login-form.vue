289102f24ed190c649a065bdd47daea7d8947810
jaxMine <304872337@qq.com>
2019-01-06 00:24:34 +0800

    dd

--- src/components/login-form/login-form.vue
+++ src/components/login-form/login-form.vue
@@ -81,7 +81,7 @@ export default {
       }
     },
     kaptchaSrc() {
-      const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
+      const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : ''
 
       return baseUrl + config.kaptchaUrl
     }
